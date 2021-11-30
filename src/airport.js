abb24dadc9f78fc3017fe27f9c7d83d1af783659
Mark Warner <mark.warner@verizon.com>
2021-11-30 16:24:28 -0600

    Add aync airport data loading

--- src/airport.js
+++ src/airport.js
@@ -1,21 +1,55 @@
+import * as fs from 'fs';
+import { readFile } from 'fs/promises';
+
 class Airport {
   static airports = [];
-  
-  constructor(name, abbreviation) {
+
+  constructor(name) {
     this.name = name;
-    this.abbreviation = abbreviation;
     this.airplanes = [];
     this.constructor.airports = [...this.constructor.airports, this];
+  }
+
+  getInfoCallback(callback) {
+    const filePath = new URL("../data/airport-data.json", import.meta.url);
+    fs.readFile(filePath, (err, data) => {
+      const airports = JSON.parse(String(data));
+      const airport = airports.find(airport => airport.iata === this.name);
+      callback(err, airport)
+    })
+  }
+
+  getInfoPromise() {
+    const filePath = new URL("../data/airport-data.json", import.meta.url);
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, (err, data) => {
+        if (err) return reject(err)
+        const airports = JSON.parse(String(data))
+        const airport = airports.find(airport => airport.iata === this.name);
+        resolve(airport)
+      })
+    })
+  }
+
+  async getInfoAsync() {
+    const filePath = new URL("../data/airport-data.json", import.meta.url);
+    try {
+      const getData = await readFile(filePath);
+      const airports = JSON.parse(String(getData));
+      const airport = airports.find(airport => airport.iata === this.name);
+      return airport;
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   land(airplane) {
     this.airplanes = [...this.airplanes, airplane];
   }
+
+  takeOff(airplaneId) {
+    this.airplanes = airplanes.filter(airplane => airplane.id !== airplaneId);
+  }
 }
 
-// const dfw = new Airport("Dallas/Fort Worth", "DFW");
-// const iah = new Airport("Houston", "IAH")
-// console.log(dfw);
-// console.log(iah);
-// console.log(Airport.airports);
 export default Airport;
--- test/airport.test.js
+++ test/airport.test.js
@@ -3,13 +3,13 @@ import Airplane from '../src/airplane';
 
 describe("Airport class", () => {
   test("Creates Airport instance", () => {
-    const dfw = new Airport("Dallas/Fort Worth", "DFW");
+    const dfw = new Airport("DFW");
     expect(dfw).toBeDefined();
     expect(dfw instanceof Airport).toBeTruthy();
   })
 
   test("can have airplanes land", () => {
-    const dfw = new Airport("Dallas/Fort Worth", "DFW");
+    const dfw = new Airport("DFW");
     const id = "1A";
     const capacity = 250;
     const range = 1000;
@@ -18,5 +18,31 @@ describe("Airport class", () => {
     dfw.land(testAirplane);
     expect(dfw.airplanes.length).toBe(1);
     expect(dfw.airplanes[0]).toBe(testAirplane);
+  })
+
+  test('airports have a city', (done) => {
+    const CDG = new Airport('CDG')
+    CDG.getInfoCallback((err, info) => {
+      expect(err).toBeNull();
+      expect(info.country).toEqual('FR');
+      done();
+    })
+  })
+
+  test('airports have a city', () => {
+    const CDG = new Airport('CDG');
+    return CDG.getInfoPromise()
+      .then(info => {
+        expect(info.city).toEqual('Paris');
+      })
+      .catch(err => {
+        expect(err).toBeNull();
+      });
+  })
+
+  test('can get information like the city from an airport instance', async () => {
+    const CDG = new Airport('CDG');
+    const airport = await CDG.getInfoAsync();
+    expect(airport.city).toEqual('Paris');
   })
 })
